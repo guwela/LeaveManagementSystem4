6ae4d5e371155036a08f9e09b06b19421a7a4915
guwela <ntwananopardon@gmail.com>
2025-07-18 17:58:13 +0200

    Add project files.

---
+++ LeaveManagementSystem4.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeaveManagementSystem4.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
