d44f305c662b1dd4d61de52fd5e47f74590f5569
guwela <ntwananopardon@gmail.com>
2025-07-22 10:54:06 +0200

    Completed Leave Management Module

--- LeaveManagementSystem4.Web/Data/ApplicationDbContext.cs
+++ LeaveManagementSystem4.Web/Data/ApplicationDbContext.cs
@@ -9,5 +9,6 @@ namespace LeaveManagementSystem4.Web.Data
             : base(options)
         {
         }
+        public DbSet<LeaveType> LeaveTypes  { get; set; }
     }
 }
---
+++ LeaveManagementSystem4.Web/Data/LeaveType.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LeaveManagementSystem4.Web.Data
+{
+    public class LeaveType
+    {
+        public int Id { get; set; }
+        [Column(TypeName = "nvarchar(150)")]
+        public string Name { get; set; }
+        public int NumberOfDay { get; set; }
+    }
+}
